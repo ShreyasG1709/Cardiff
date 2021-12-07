43c7cc0239ff04eaee609c7a46fd1c3f9d70010e
Shreyas Ghorpade <36772206+ShreyasG1709@users.noreply.github.com>
2021-12-07 15:39:56 +0000

    Add files via upload

---
+++ DSCW2 final.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+def pluralize(s):    
+    a = {}
+    words = []
+    with open('eg.txt') as f:
+        for line in f:
+            words.append(line.strip())
+        if s.lower() in words:
+            a = {"plural": s , "status" : "Proper_Noun"}
+            return a
+        elif s == '':
+            a = {"plural": "" , "status" : "Empty_String"}
+            return a
+        elif s[-1] == 's':
+            a = {"plural": s , "status" : "already_in_plural"}
+            return a
+        else:
+            if s[-1] in 'a,e,i,o,u':
+                s = s + 's'
+                a = {"plural": s , "status" : "already_in_plural"}
+                return a
+            elif s[-1] in '0,1,2,3,4,5,6,7,8,9':
+                a = {"plural": s , "status" : "ending_with_number"}
+                return a
+            elif s[-1] == 'y' and s[-2] not in 'a,e,i,o,u':
+                s=s.replace(s[-1], 'ies')
+                a = {"plural": s , "status" : "success"}
+                return a
+            elif s[-1] == 'f' and s not in words:
+                s=s.replace(s[-1], 'ves')    
+               # a["plural"] = s
+                #a["status"] = "success"
+                a = {"plural": s , "status" : "success"}
+                return a
+            elif s[-2:] in 'sh , ch, z':
+                s = s + 'es'
+                a = {"plural": s , "status" : "success"}
+                return a
+            else:
+                s+='s'
+                a = {"plural": s , "status" : "success"}
+                return a
+    pass
+
+
+# In[10]:
+
+
+TEST_CASES = """failure
+food
+Zulma
+injury
+elf
+buzz
+computers
+PCs
+
+highway
+presentation
+pouch
+COVID-19
+adam""".split('\n')
+
+if __name__ == '__main__':
+
+  for test_noun in TEST_CASES:
+
+    print(test_noun,'-->',pluralize(test_noun))
+    print('----')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ DSCW3 final.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[242]:
+
+
+import re
+def function_renamer(code):
+    d = {}
+    ind = 0
+    match = re.findall(r'def ([\w]+)', code)
+    match.sort()
+    for words in match:
+        if words[0] == "_":
+            code_n = code.replace(str(words.lstrip('_')), "".join(words.title().split("_")))   
+        elif '_' in words[1:]:
+            code_n = code.replace(words, "".join(words.title().split("_")))
+        else:
+            words = words
+            
+    match1 = re.findall(r'def ([\w]+)', code_n)
+    match1.sort()
+    for i in match:
+        d[i] = {'hash' : hash(i), 'camelcase': match1[ind], 'allcaps' : i.upper()}
+        ind+=1
+        
+    print(d,"\n", code_n)
+
+
+# In[244]:
+
+
+code='''
+def _major_split(*args):
+    return (args[:2], args[2:]) 
+    
+def CheckTruth(t = True):
+    print(’t is’, t)
+    return _major_split([t]*10)   
+    
+x, y = _major_split((10, 20, 30, 40, 50))
+CheckTruth(len(x) == 10)
+'''
+
+code1 ='''
+def add_two_numbers(a, b):
+    return a + b
+    
+print(add_two_numbers(10, 20))
+'''
+
+function_renamer(code)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
